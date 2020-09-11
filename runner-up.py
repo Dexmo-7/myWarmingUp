b6ff3775230ab22817ded4925ba0a6e460b6f95a
Mateusz Owsianka <mowsianka13@gmail.com>
2020-09-11 13:12:08 +0200

    Program shows the second highest entered value.

---
+++ runner-up.py
@@ -0,0 +1,13 @@
+﻿def runner_up():
+    try:
+        arr = map(int, input().split(" "))
+        print(sorted(list(set(arr)))[-2])
+    except (TypeError, ValueError):
+        print("Caught TypeError or ValueError!")
+    finally:
+        print("Function was executed...")
+
+print("Program show the second highest entered value.")
+print("Please provide the numbers (please use spaces as split marks):")
+
+runner_up()
