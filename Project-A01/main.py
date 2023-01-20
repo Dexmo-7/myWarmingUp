aecb3d34257f349cce43272eea66ad566bc5e9dd
Mateusz <mowsianka13@gmail.com>
2023-01-20 13:42:22 +0100

    Commit just for checking the functionality of pyCharm (and my skill...)

---
+++ Project-A01/main.py
@@ -0,0 +1,4 @@
+print('In this project we create "idea generator", which will be helpful for new story designing.')
+print('The main idea is to create window-based application with several objects such as: "Hero", "Villain", etc.')
+print('Objects will be random-selected from .csv file - probably from tuples.')
+print('The window design should be prepared in Qt Design Studio. Just for fun.')
